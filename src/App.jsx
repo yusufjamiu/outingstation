d2d516d7a383ef4f84b8b7592491e4886dd88103
mac <mac@macs-MacBook-Pro-948.local>
2026-03-25 18:00:22 +0300

    Changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
 import { SavedEventsProvider } from './context/SavedEventsContext';
 import ToastContainer from './components/ToastContainer';
